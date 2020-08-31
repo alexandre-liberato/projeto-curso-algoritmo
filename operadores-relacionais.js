17b4a3679e9746c695ea3ddc87d1b847713f5367
Alexandre Liberato <sdkweb@hotmail.com>
2020-08-31 19:52:49 -0300

    Esse logaritmo demonstra os diferentes tipos de operadores relacionais

---
+++ operadores-relacionais.js
@@ -0,0 +1,11 @@
+// Esse algoritmo compara se dois valores inseridos nos popups são iguais ou diferentes
+
+var nota1, nota2;
+
+nota1 = prompt("Digite a primeira nota")
+nota2 = prompt("Digite a segunda nota")
+
+if(nota1 == nota2)
+    alert("Nota 1 é igual a nota 2")
+else
+    alert("Nota 1 é diferente de nota 2")
