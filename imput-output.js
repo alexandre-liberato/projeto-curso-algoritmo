0d3f4e7cfd0034584dbbc6bda0e21bd39c157ce4
Alexandre Liberato <sdkweb@hotmail.com>
2020-08-31 19:49:05 -0300

    Esse algoritmo demonstra a entrada e saída de dados

---
+++ imput-output.js
@@ -0,0 +1,8 @@
+// Esse algoritmo retorna ao usuário o que ele digitou no popup
+
+var nome, idade;
+
+nome = prompt("Digite o seu nome: ")
+idade = prompt("Digite a sua idade: ")
+alert("O nome digitado foi: " + nome)
+alert("A idade digitada foi: " + idade)
